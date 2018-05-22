88f11786a1b220041b558470af1f25bec2cab024
reza <reza@gmail.com>
2018-05-22 16:54:30 +0430

    scrollView

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Text, StyleSheet, Platform, Dimensions} from 'react-native';
 
 export default class Article extends Component{
 
     render() {
+        const {title, imageUrl, body} = this.props;
         return (
-            <View>
-                <Text>سیدرضا حسینی</Text>
-                <Text style={{fontFamily: 'IRANSansMobile_Bold'}}>سیدرضا حسینی</Text>
-                <Text style={{fontFamily: 'IRANSansMobile_UltraLight'}}>سیدرضا حسینی</Text>
+            <View style={styles.container}>
+                <Image source={imageUrl} style={styles.image}/>
+                <View style={{padding: 10}}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text numberOfLines={5} style={styles.body}>{body}</Text>
+                </View>
             </View>
         )
     }
 }
+
+const { width , height } = Dimensions.get('window');
+const gutter = 10;
+
+const styles = StyleSheet.create({
+    container: {
+        width : ( width - gutter * 3) / 2,
+        backgroundColor: 'white',
+        elevation: 1,
+        marginBottom: 10,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 20},
+        shadowOpacity: 0.2
+    },
+    image: {
+        width: '100%',
+        height: 120
+    },
+    title: {
+        textAlign: 'right',
+        fontSize: 16,
+        ...Platform.select({
+            ios: {
+                fontFamily: 'IRANSansMobile',
+                fontWeight: 'bold'
+            },
+            android: {
+                fontFamily: 'IRANSansMobile_Bold'
+            }
+        })
+    },
+    body: {
+        textAlign: 'right',
+        fontSize : 12,
+        ...Platform.select({
+            ios: {
+                fontFamily: 'IRANSansMobile',
+                fontWeight: 'bold'
+            },
+            android: {
+                fontFamily: 'IRANSansMobile_Bold'
+            }
+        })
+    }
+});
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import Article from './Article'
+import Article from './Article';
+import { ScrollView, StyleSheet } from 'react-native';
 
 export default class Articles extends Component{
 
     render() {
         return (
-            <Article/>
+            <ScrollView contentContainerStyle={styles.container}>
+                {this.props.articles.map((item, index) => <Article {...item} key={index}/>)}
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent : 'space-between',
+        margin : 10
+    }
+});
